3637785bec14c23ec076df18c6efa9efdf720027
Surbhi-0204 <surbhik531@gmail.com>
2025-02-22 02:30:42 +0530

    Data Types and ECMA Standard

---
+++ Day1.js
@@ -0,0 +1,28 @@
+"use strict "// treats all js code as newer version 
+//alert("abxsbchidc") // alert() is a functionality provided by the browser, not JavaScript itself.
+        /*alert() is part of the Window API, which is provided by web browsers.
+        JavaScript, as a language, does not define alert(), but browsers implement it as part of the Global window object.
+        This means alert() will not work in non-browser environments like Node.js because Node.js does not have a window object.*/
+console.log(3 
+    +3)// can be written but code readability should be high
+console.log("Surbhi");
+
+///Data-Types
+
+let name = "Surbhi" 
+let age = 25
+let isLoggedIn = false
+let state = undefined
+
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+// undefined =>
+//symbol => unique  mostly used in React, Figma tool and so on
+
+//Object 
+
+console.log(typeof undefined)  //undefined
+console.log(typeof null)  // null n null is an object 
