3f8e69fac545e26c688d4bd30336c1ca54d73583
Surbhi-0204 <surbhik531@gmail.com>
2025-02-24 00:47:52 +0530

    Comparison

--- Day2.js
+++ Day2.js
@@ -31,3 +31,38 @@ let  booleanIsLoggedIn= Boolean(isLoggedIn);
 
 let someNumber= 24
 let stringNumber= String(someNumber);
+
+// ***************************************Operations***************************************************************************//
+let value2=3
+let negvalue=-value2 //-3
+
+let str = "hello"
+let str1 = " Surbhi"
+let str2 = str + str1 // "hello Surbhi"
+
+console.log("1"+ 2) //12
+console.log(1+ "2") //12
+console.log("1"+ 2 + 2) //122
+console.log(1+ 2 + "2") //32
+
+console.log(+true) //1   
+console.log(+"") //0
+
+//prefix and postfix
+let counter=10
+++counter //prefix
+counter++ //postfix
+
+//Comparision
+console.log("2">1);
+console.log("02">1); // two value of different data types should not be compared
+
+console.log(null>0); //false
+console.log(null==0); //false
+console.log(null>=0); //true
+console.log (undefined==0); //false
+console.log(undefined>0); //false  
+// should avoid these type of comparision
+
+//strict comparison
+console.log("2"===2); //doesnt start conversion as the datatype is different
